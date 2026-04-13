342f5eac3a254ce11ef30f9c7711a105fc6dd434
unknown <slava.shamne.20@gmail.com>
2026-04-14 01:12:13 +0700

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { getServerSession } from '../lib/auth';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
-
+  // Проверяем авторизацию
   const session = await getServerSession();
   if (!session) {
     redirect('/login');
